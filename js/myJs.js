22211b228de6a4f4f4b54a5b7f84e4b8494a5529
hùng hà huy <83862705+huyhung2001@users.noreply.github.com>
2021-08-18 23:43:27 +0700

    upload

--- js/myJs.js
+++ js/myJs.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
 
   // switch button position
   function switchButton() {
-    var audio = new Audio("sound/nhac.mp3");
+    var audio = new Audio("sound/duck.mp3");
     audio.play();
     var leftNo = $("#no").css("left");
     var topNO = $("#no").css("top");
