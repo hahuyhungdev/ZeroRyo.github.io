c47ba7a3d9035077fffa7125247c28bf9023bfe0
hùng hà huy <83862705+huyhung2001@users.noreply.github.com>
2021-08-18 23:47:02 +0700

    upload

--- js/myJs.js
+++ js/myJs.js
@@ -107,7 +107,7 @@ $(document).ready(function () {
 
   // show popup
   $("#yes").click(function () {
-    var audio = new Audio("sound/tick.mp3");
+    var audio = new Audio("sound/nhac.mp3");
     audio.play();
     Swal.fire({
       title: textConfig.text7,
